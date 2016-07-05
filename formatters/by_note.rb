edbeb3d4a8c645f979ae1bda06323b1e4b7d21a4
Alexey Shumkin <Alex.Crezoff@gmail.com>
2016-07-06 00:30:57 +0300

    improve: by_note formatter: change visualization
    Duration first, then notes.
    Fixed separators.

--- formatters/by_note.rb
+++ formatters/by_note.rb
@@ -15,37 +15,35 @@ module Timetrap
           h
         end
 
-        id_heading = Timetrap::CLI.args['-v'] ? 'Id ' : ''
-        longest_sheet = entries.inject('Sheet'.length) {|l, e| [e.sheet.rstrip.length, l].max}
+        id_heading = Timetrap::CLI.args['-v'] ? 'Id' : ''
         longest_note = entries.inject('Notes'.length) {|l, e| [e.note.rstrip.length, l].max}
 
+        dash_len = 'Duration - '.length
         by_date.keys.sort.each do |date|
           self.output << "## #{format_date date} ##\n\n"
 
-          self.output << "                 Notes  Duration\n" % (' '*(longest_sheet-5))
-
-          last_sheet = nil
+          self.output << "%3s Duration   Notes\n" % id_heading
 
           by_date[date].group_by(&:note).each do |k, v|
             duration = v.map(&:duration).reduce(:+)
             line     = v.first
 
-            self.output <<  "%-4s - %15s %s\n" % [
+            self.output <<  "%3s %8s - %-15s\n" % [
               (Timetrap::CLI.args['-v'] ? line.id : ''),
-              line.note,
               format_duration(duration),
+              line.note,
             ]
           end
 
-          self.output << "    %s\n" % ('─'*(40+longest_sheet + longest_note))
-          self.output << "    Total%43s\n" % format_total(by_date[date])
+          self.output << "    %s\n" % ('─' * (dash_len + longest_note))
+          self.output << "    Total: %12s\n" % format_total(by_date[date])
           self.output << "\n"
 
         end
 
         if by_date.size > 1
-          self.output <<  "%s\n" % ('─'*(4+40+longest_sheet + longest_note))
-          self.output <<  "Grand Total%41s\n" % format_total(by_date.values.flatten)
+          self.output <<  "%s\n" % ('─' * ('##  '.length + dash_len + longest_note))
+          self.output <<  "Grand Total: %10s\n" % format_total(by_date.values.flatten)
         end
       end
     end
