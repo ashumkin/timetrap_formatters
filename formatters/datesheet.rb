f1a1c620ead260a348ee35247a1b5cac1357756f
Alexey Shumkin <Alex.Crezoff@gmail.com>
2016-07-06 01:05:53 +0300

    improve: datesheet formatter: sort by date
    Not by sheet name.

--- formatters/datesheet.rb
+++ formatters/datesheet.rb
@@ -1,49 +1,36 @@
 module Timetrap
   module Formatters
-  class Timetrap::Formatters::Datesheet
+    class Timetrap::Formatters::Datesheet
       attr_accessor :output
       include Timetrap::Helpers
 
-	  def time_format
-			  "%Y-%m-%d"
-	  end	  
+      def time_format
+        "%Y-%m-%d"
+      end
 
       def initialize entries
         self.output = ''
-        sheets = entries.inject({}) do |h, e|
-          h[e.sheet] ||= []
-          h[e.sheet] << e
+        by_date = entries.inject({}) do |h, e|
+          date = e.start.to_date
+          h[date] ||= []
+          h[date] << e
           h
         end
-        (sheet_names = sheets.keys.sort).each do |sheet|
-          id_heading = Timetrap::CLI.args['-v'] ? 'Id' : '  '
-          last_start = nil
-          from_current_day = []
-          sheets[sheet].each_with_index do |e, i|
-            from_current_day << e
-           e_end = e.end_or_now
-            self.output <<  "%1s%10s%11s -%9s%10s  [%s]  %s\n" % [
+
+        by_date.keys.sort.each do |date|
+          by_date[date].each_with_index do |e, i|
+            self.output <<  "%3s %10s%11s -%9s%10s  [%s]  %s\n" % [
               (Timetrap::CLI.args['-v'] ? e.id : ''),
               e.start.strftime(time_format),
-			  format_time(e.start),
+              format_time(e.start),
               format_time(e.end),
-             format_duration(e.duration),
+              format_duration(e.duration),
               e.sheet,
-			  e.note
+              e.note
             ]
-
-            nxt = sheets[sheet].to_a[i+1]
-            if nxt == nil or !same_day?(e.start, nxt.start)
-              from_current_day = []
-            else
-            end
-            last_start = e.start
           end
         end
-        if sheets.size > 1
-       	end
-        
-	  end
+      end
     end
   end
 end
